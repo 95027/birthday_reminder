e7cec1b6c0969715241e15b1eb0eb87cf2fdb708
Ch. Sai Kumar <96567025+95027@users.noreply.github.com>
2023-05-06 12:04:55 +0530

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,36 @@
+import Larry from './assets/Bertie.jpg';
+import Hester from './assets/Hester.jpg';
+import Bertie from './assets/Larry.jpg';
+import Lola from './assets/Lola.jpg';
+import Sean from './assets/Sean.jpg';
+
+export default [
+    {
+        id: 1,
+        name: 'Bertie Yates',
+        age: 29,
+        image:Bertie
+    },
+    {
+        id: 2,
+        name: 'Hester Hogan',
+        age: 32,
+        image:Hester
+    },
+    {
+        id: 3,
+        name: 'Larry Little',
+        age: 39,
+        image:Larry
+    },    {
+        id: 4,
+        name: 'Sean Walsh',
+        age: 19,
+        image:Sean
+    },    {
+        id: 5,
+        name: 'Lola Gardener',
+        age: 34,
+        image:Lola
+    }
+]
